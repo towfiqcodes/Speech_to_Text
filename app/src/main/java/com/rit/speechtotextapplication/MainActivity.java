133f0515f86fd595927fa94de16e697e9e9111d8
towfiqulislam <hridoy.islam7916@gmail.com>
2018-10-14 00:21:46 +0600

    speeak something

---
+++ app/src/main/java/com/rit/speechtotextapplication/MainActivity.java
@@ -0,0 +1,62 @@
+package com.rit.speechtotextapplication;
+
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.speech.RecognizerIntent;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView voiceInput;
+  private ImageView micButton;
+  private final int REQ_CODE_VOICE_INPUT=100;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_main );
+        voiceInput=findViewById( R.id.voiceInput );
+        micButton=findViewById( R.id.btnMic );
+        micButton.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getVoiceInput();
+            }
+        } );
+    }
+
+    private void getVoiceInput() {
+        Intent intent=new Intent( RecognizerIntent.ACTION_RECOGNIZE_SPEECH );
+        intent.putExtra( RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM );
+        intent.putExtra( RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra( RecognizerIntent.EXTRA_PROMPT,"Please speak Something" );
+
+        try {
+            startActivityForResult( intent,REQ_CODE_VOICE_INPUT );
+        } catch (ActivityNotFoundException e) {
+            Toast.makeText( getApplicationContext(),
+                            e.getMessage(),
+                            Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult( requestCode, resultCode, data );
+
+        switch (requestCode){
+            case REQ_CODE_VOICE_INPUT:
+                if(resultCode==RESULT_OK && null !=data){
+                    ArrayList<String> rresult=data.getStringArrayListExtra( RecognizerIntent.EXTRA_RESULTS );
+                    voiceInput.setText( rresult.get(0) );
+                }
+        }
+    }
+}
